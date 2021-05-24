242fedf986daf92f8d5f9d6717f73e73ac4b9955
Mariia Ros <rosm0099@gmail.com>
2021-05-24 13:11:33 +0200

    using trim() in tasks 5,6.

--- goit-js-hw-07/js/task-05.js
+++ goit-js-hw-07/js/task-05.js
@@ -3,7 +3,7 @@ const outputRef = document.getElementById("name-output");
 inputRef.addEventListener("input", report);
 
 function report() {
-    const text = inputRef.value;
-    console.log("report ", text);
-    outputRef.textContent = (text === "") ? "незнакомец": text;
+    const text = inputRef.value.trim();
+    //console.log("report ", text);
+    outputRef.textContent = (text === "") ? "незнакомец" : text;
 }
--- goit-js-hw-07/js/task-06.js
+++ goit-js-hw-07/js/task-06.js
@@ -2,7 +2,7 @@ const inputRef = document.getElementById("validation-input");
 inputRef.addEventListener("input", onChange);
 
 function onChange() {
-    const text = inputRef.value;
+    const text = inputRef.value.trim();
     const lengthRequired = parseInt(inputRef.getAttribute("data-length"));
     inputRef.classList.remove("valid", "invalid");
     if (text.length === lengthRequired) {
