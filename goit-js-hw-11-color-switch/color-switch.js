142a0a677963094386946e6eb46a93585859de92
Mariia Ros <rosm0099@gmail.com>
2021-06-26 13:06:51 +0200

    buttons disabled/enabled

--- goit-js-hw-11-color-switch/color-switch.js
+++ goit-js-hw-11-color-switch/color-switch.js
@@ -17,14 +17,20 @@ const buttonStopRef = document.querySelector('button[data-action="stop"]');
 
 let timerRetValue = null;
 
+buttonStopRef.disabled = true;
+
 buttonStartRef.addEventListener('click', () => {
+    buttonStopRef.disabled = false;
+    buttonStartRef.disabled = true;
     timerRetValue = setInterval(() => {
         const color = colors[randomIntegerFromInterval(0, colors.length - 1)];
         bodyRef.setAttribute('style', `background-color: ${color};`);
-    }, 1000);
+      }, 1000);
 })
 
 buttonStopRef.addEventListener('click', () => {
+    buttonStopRef.disabled = true;
+    buttonStartRef.disabled = false;
     if (timerRetValue != null) {
         clearInterval(timerRetValue);
         timerRetValue = null;
