da658294a09a2cba2f54806ca8c0e7a626c42dd4
Mariia Ros <rosm0099@gmail.com>
2021-07-09 14:07:25 +0200

    corrected functionality
    pictures desapear on query with no answer

--- goit-js-hw-13-image-finder/src/searchImages.js
+++ goit-js-hw-13-image-finder/src/searchImages.js
@@ -21,60 +21,61 @@ function clearUl(ulRef) {
 }
 
 function formatQuery(text) {
-    return text.split(' ').join('+');
+  return text.split(' ').join('+');
 }
 
 function addPhotos(hits) {
-    let i = 0;
-    for (const hit of hits) {
-        htmlText += `<li id="card${photosCount+i}"><div class="photo-card"><img src="${hit.webformatURL}" alt="" />`;
-        htmlText += template(hit);
-        htmlText += '</div></li>';
-        i += 1;
-    }
+  let i = 0;
+  for (const hit of hits) {
+    htmlText += `<li id="card${photosCount + i}"><div class="photo-card"><img src="${
+      hit.webformatURL
+    }" alt="" />`;
+    htmlText += template(hit);
+    htmlText += '</div></li>';
+    i += 1;
+  }
 }
 
 function showPhotos() {
   debounce(() => {
     apiService(queryText, page, perPage)
-        .then(apiOutput => {
-            console.log(apiOutput);
-            if (apiOutput.hits.length > 0) {
-                addPhotos(apiOutput.hits);
-                clearUl(galleryRef);
-                galleryRef.insertAdjacentHTML("beforeend", htmlText);
-                const element = document.getElementById(`card${photosCount}`);
-                element.scrollIntoView({
-                    behavior: 'smooth',
-                    block: 'end',
-                });
-                photosCount += apiOutput.hits.length;
-            }
-        if (apiOutput.hits.length >= perPage) {
-            page += 1;
-            buttonNextRef.disabled = false;
-        } else {
-            buttonNextRef.disabled = true;
+      .then(apiOutput => {
+        //console.log(apiOutput);
+        clearUl(galleryRef);
+        if (apiOutput.hits.length > 0) {
+          addPhotos(apiOutput.hits); /*adds to htmlText*/
+          galleryRef.insertAdjacentHTML('beforeend', htmlText);
+          const element = document.getElementById(`card${photosCount}`);
+          element.scrollIntoView({
+            behavior: 'smooth',
+            block: 'end',
+          });
+          photosCount += apiOutput.hits.length;
         }
-    })
-    .catch(function (error) {
-      console.log('Error: ' + error);
-    });
+        if (apiOutput.hits.length >= perPage) {
+          page += 1;
+          buttonNextRef.disabled = false;
+        } else {
+          buttonNextRef.disabled = true;
+        }
+      })
+      .catch(function (error) {
+        console.log('Error: ' + error);
+      });
   }, 500)();
 }
 
 inputRef.addEventListener('change', () => {
-    //clearUl(galleryRef);
-    page = 1;
-    photosCount = 0;
-    queryText = formatQuery(inputRef.value.trim());
-    htmlText = '';
-    showPhotos();
+  //clearUl(galleryRef);
+  page = 1;
+  photosCount = 0;
+  queryText = formatQuery(inputRef.value.trim());
+  htmlText = '';
+  showPhotos();
 });
 
 buttonNextRef.addEventListener('click', () => {
-    showPhotos();
+  showPhotos();
 });
 
 buttonNextRef.disabled = true;
-
